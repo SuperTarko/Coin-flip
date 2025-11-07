c315583754ea0366c08a9ca4f5fbf8630911fe56
SuperTarko <tarik.prelic@gmail.com>
2025-11-07 18:18:17 +0100

    Coin-flip

---
+++ app.js
@@ -0,0 +1,41 @@
+let score = JSON.parse(localStorage.getItem("score")) || {
+  wins: 0,
+  losses: 0,
+};
+
+function coinFlip() {
+  const flip = Math.random();
+
+  let flipResult = "";
+
+  if (flip < 0.5) {
+    flipResult = "heads";
+  } else if (flip >= 0.5) {
+    flipResult = "tails";
+  }
+
+  return flipResult;
+}
+
+function game(playerChoice) {
+  const flipResult = coinFlip();
+
+  let result = "";
+
+  if (flipResult === playerChoice) {
+    result = "You win!";
+  } else if (flipResult !== playerChoice) {
+    result = "You lose!";
+  }
+
+  if (result === "You win!") {
+    score.wins += 1;
+  } else if (result === "You lose!") {
+    score.losses += 1;
+  }
+
+  localStorage.setItem("score", JSON.stringify(score));
+
+  alert(`The flip was ${flipResult}. ${result}
+Wins: ${score.wins}, Losses ${score.losses}`);
+}
